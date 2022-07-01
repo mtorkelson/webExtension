6dfc2ed3051a0ff671f6c531c5dc046582e0d362
Russ K <russell@directscale.com>
2022-06-30 18:15:35 -0600

    Added AddDirectScale to Startup

--- WebExtension/Startup.cs
+++ WebExtension/Startup.cs
@@ -25,6 +25,7 @@ namespace WebExtension
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDirectScale();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
